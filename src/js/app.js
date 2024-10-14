84a32676e1cdbf863699e2046e3aebe34a919e10
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-14 14:42:40 +0000

    Objetivo principal finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -27,20 +27,75 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.includeCover == false) {
+    console.log(`${variables.city}`);
+    cover = "<div class='cover'></div>";
+  }
+
+  // ---
+
+  let role = `${variables.role}`;
+  if (variables.role === null) role = "Web Developer";
+
+  // ---
+
+  let city = `${variables.city}`;
+  if (variables.city === null) city = "Miami";
+
+  let country = `${variables.country}`;
+  if (variables.country === null) country = "USA";
+
+  let place = (city + ", " + country).trimStart();
+
+  // ---
+
+  let name = `${variables.name}`;
+  if (variables.name === null) name = "";
+
+  let lastName = `${variables.lastName}`;
+  if (variables.lastName === null) lastName = "";
+
+  let nameCompleto = (name + " " + lastName).trimStart();
+  if (variables.name == null && variables.lastName == null)
+    nameCompleto = "Lucy Boilett";
+
+  // ---
+
+  let smPos = `${variables.socialMediaPosition}`;
+  if (variables.socialMediaPosition === null) smPos = "right";
+
+  // ---
+
+  let twitter = `${variables.twitter}`;
+  if (variables.twitter === null) twitter = "https://twitter.com/4geeksacademy";
+
+  let github = `${variables.github}`;
+  if (variables.github === null) github = "https://github.com/4geeksacademy";
+
+  let linkedin = `${variables.linkedin}`;
+  if (variables.linkedin === null)
+    linkedin = "https://linkedin.com/school/4geeksacademy";
+
+  let instagram = `${variables.instagram}`;
+  if (variables.instagram === null)
+    instagram = "https://instagram.com/4geeksacademy";
+
+  // ---
+  // ---
+  // ---
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+            ${cover} 
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1> ${nameCompleto} </h1>
+          <h2> ${role} </h2>
+          <h3> ${place} </h3>
+          <ul class=${smPos}>
+          <li><a href=${twitter}><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${github}><i class="fab fa-github"></i></a></li>
+            <li><a href=${linkedin}><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${instagram}><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
